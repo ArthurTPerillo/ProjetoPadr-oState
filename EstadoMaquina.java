abe0fa1f8bfe6ab9158a59d1281496c706f3c2ad
ARTHUR TEIXEIRA PERILLO <109046338+ArthurTPerillo@users.noreply.github.com>
2024-12-15 01:54:01 -0300

    Create EstadoMaquina.java

---
+++ EstadoMaquina.java
@@ -0,0 +1,5 @@
+public interface EstadoMaquina {
+    void inserirMoeda(MaquinaVendas maquina);
+    void selecionarProduto(MaquinaVendas maquina);
+    void dispensarProduto(MaquinaVendas maquina);
+}
